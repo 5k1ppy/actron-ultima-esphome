40ff6dfc4752c484f6a847b91459ad40d2b949b3
Skippy <82199824+5k1ppy@users.noreply.github.com>
2026-08-16 15:47:40 +1000

    Update sensor.py

--- components/actron_ultima/sensor.py
+++ components/actron_ultima/sensor.py
@@ -5,6 +5,7 @@ from esphome.components import sensor
 
 from . import ActronUltima
 
+DEPENDENCIES = ["actron_ultima"]
 
 CONF_ACTRON_ULTIMA_ID = "actron_ultima_id"
 CONF_SETPOINT_TEMP = "setpoint_temp"
