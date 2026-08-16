c0f58f66a68a25fc759c084c43de9ca5520fc661
Skippy <82199824+5k1ppy@users.noreply.github.com>
2026-08-16 14:37:39 +1000

    Update text_sensor.py

--- components/actron_ultima/text_sensor.py
+++ components/actron_ultima/text_sensor.py
@@ -2,16 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 from esphome.components import text_sensor
-from esphome.const import CONF_ID
 
 from . import ActronUltima
 
 
+CONF_ACTRON_ULTIMA_ID = "actron_ultima_id"
 CONF_BIT_STRING = "bit_string"
+
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.use_id(ActronUltima),
+        cv.Required(CONF_ACTRON_ULTIMA_ID):
+            cv.use_id(ActronUltima),
 
         cv.Required(CONF_BIT_STRING):
             text_sensor.text_sensor_schema(),
@@ -20,10 +22,14 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_ID])
+    parent = await cg.get_variable(
+        config[CONF_ACTRON_ULTIMA_ID]
+    )
 
     sens = await text_sensor.new_text_sensor(
         config[CONF_BIT_STRING]
     )
 
-    cg.add(parent.set_bit_string_sensor(sens))
+    cg.add(
+        parent.set_bit_string_sensor(sens)
+    )
