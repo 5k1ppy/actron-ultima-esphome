da5d038adc5aafe84de07fac7e8db454768cde6e
Skippy <82199824+5k1ppy@users.noreply.github.com>
2026-08-16 14:30:29 +1000

    Create text_sensor.py

---
+++ components/actron_ultima/text_sensor.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.components import text_sensor
+
+from . import ActronUltima, CONF_ACTRON_ULTIMA_ID
+
+
+DEPENDENCIES = ["actron_ultima"]
+
+CONF_BIT_STRING = "bit_string"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_ACTRON_ULTIMA_ID): cv.use_id(ActronUltima),
+
+        cv.Optional(CONF_BIT_STRING):
+            text_sensor.text_sensor_schema(),
+    }
+)
+
+
+async def to_code(config: dict[str, Any]) -> None:
+    parent = await cg.get_variable(
+        config[CONF_ACTRON_ULTIMA_ID]
+    )
+
+    if CONF_BIT_STRING in config:
+        sens = await text_sensor.new_text_sensor(
+            config[CONF_BIT_STRING]
+        )
+
+        cg.add(
+            parent.set_bit_string_sensor(sens)
+        )
